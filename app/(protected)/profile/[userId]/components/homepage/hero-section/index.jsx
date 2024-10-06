adf03d0a23878eb96bdd7eb809c22231af65e481
Saikat123Dev <berasaikat729@gmail.com>
2024-10-06 20:19:04 +0530

    connect request send

--- app/(protected)/profile/[userId]/components/homepage/hero-section/index.jsx
+++ app/(protected)/profile/[userId]/components/homepage/hero-section/index.jsx
@@ -6,17 +6,17 @@ import Image from "next/image";
 import Link from "next/link";
 import { BsGithub, BsLinkedin } from "react-icons/bs";
 import { FaFacebook, FaTwitterSquare } from "react-icons/fa";
-
 import { RiContactsFill } from "react-icons/ri";
 import { SiLeetcode } from "react-icons/si";
 import { useState } from "react";
-
+import { useCurrentUser } from "../../../../../../../hooks/use-current-user"
 function HeroSection() {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [message, setMessage] = useState("");
   const [purpose, setPurpose] = useState("collaboration");
   const [skills, setSkills] = useState("");
-
+  const [receiverId, setReceiverId] = useState("user_id_here"); // Set the receiver ID
+   const session = useCurrentUser()
   const handleDropdownToggle = () => {
     setIsDropdownOpen(!isDropdownOpen);
   };
@@ -29,10 +29,27 @@ function HeroSection() {
   const handlePreview = () => {
     alert(`Message: ${message}\nPurpose: ${purpose}\nSkills: ${skills}`);
   };
-  const connectRequest=()=>{
-    alert("Connection request sent!");
 
-  }
+  const connectRequest = async () => {
+   
+    const senderId = session.id
+    try {
+      const response = await fetch('/api/connect', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ receiverId: "66ffbe8d8c71a5afa10f17db" , senderId}),
+      });
+
+      if (response.ok) {
+        alert('Connection request sent successfully!');
+      } else {
+        alert('Failed to send connection request.');
+      }
+    } catch (error) {
+      alert('Error sending connection request.');
+      console.error(error);
+    }
+  };
 
   return (
     <section className="relative flex flex-col items-center justify-between py-4 lg:py-12">
@@ -93,107 +110,90 @@ function HeroSection() {
 
           <div className="flex ml-32 items-center gap-3">
             <div className="relative inline-block text-left">
-            <div className="flex items-center bg-gradient-to-r to-pink-500 from-violet-600 p-[1px] rounded-full">
-  {/* CONNECT Button */}
-  <button
-    className="px-3 text-xs md:px-8 py-3 md:py-4 bg-gradient-to-r to-pink-500 from-violet-600 rounded-l-full border-none text-center md:text-sm font-medium uppercase tracking-wider text-white no-underline transition-all duration-200 ease-out md:font-semibold"
-    id="connectButton"
-    onClick={connectRequest}
-  >
-    CONNECT
-  </button>
+              <div className="flex items-center bg-gradient-to-r to-pink-500 from-violet-600 p-[1px] rounded-full">
+                {/* CONNECT Button */}
+                <button
+                  className="px-3 text-xs md:px-8 py-3 md:py-4 bg-gradient-to-r to-pink-500 from-violet-600 rounded-l-full border-none text-center md:text-sm font-medium uppercase tracking-wider text-white no-underline transition-all duration-200 ease-out md:font-semibold"
+                  id="connectButton"
+                  onClick={connectRequest}
+                >
+                  CONNECT
+                </button>
 
-  {/* Divider Line */}
-  <div className="w-[1px] h-8 bg-white"></div>
+                {/* Divider Line */}
+                <div className="w-[1px] h-8 bg-white"></div>
 
-  {/* Dropdown Icon Button */}
-  <button
-    className="p-3 md:py-4  rounded-r-full border-none text-center  transition-all duration-200 ease-out flex items-center justify-center"
-    onClick={handleDropdownToggle}
-  >
-    <RiContactsFill size={16} className="text-white" />
-  </button>
-</div>
+                {/* Dropdown Icon Button */}
+                <button
+                  className="p-3 md:py-4  rounded-r-full border-none text-center  transition-all duration-200 ease-out flex items-center justify-center"
+                  onClick={handleDropdownToggle}
+                >
+                  <RiContactsFill size={16} className="text-white" />
+                </button>
+              </div>
 
-
-
-{isDropdownOpen && (
-  <div
-    id="dropdown"
-    className="absolute right-0 z-10 mt-2 w-64 rounded-lg shadow-xl bg-gradient-to-br from-indigo-50 via-white to-indigo-100 ring-1 ring-indigo-200 ring-opacity-50 transition-transform duration-300 transform scale-95 origin-top-right"
-  >
-    <div className="py-4 px-5 bg-white bg-opacity-90 rounded-lg">
-      <form
-        id="connectionForm"
-        className="p-4 bg-white bg-opacity-90 rounded-lg shadow-md border border-gray-200"
-        onSubmit={handleFormSubmit}
-      >
-        <textarea
-          id="message"
-          rows="4"
-          maxLength="300"
-          placeholder="Provide a brief description of the project..."
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          className="w-full p-3 border border-gray-300 rounded-lg mt-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-        ></textarea>
-        <div className="mt-4">
-          <label className="block text-sm font-medium text-gray-700">
-           Collsborstion Purpose
-          </label>
-          <select
-            id="purpose"
-            value={purpose}
-            onChange={(e) => setPurpose(e.target.value)}
-            className="mt-1 block w-full border border-gray-300 rounded-lg p-2 bg-white focus:outline-none focus:ring-2 focus:ring-indigo-500"
-          >
-            <option value="collaboration">Open-Source Project</option>
-            <option value="skill-sharing">Hackathon Team</option>
-            <option value="mentorship">Startup</option>
-            <option value="networking">Research</option>
-            <option value="discussion">Others</option>
-          </select>
-        </div>
-       <div className="mt-4">
-          <label className="block text-sm font-medium text-gray-700">
-            Skills or Topics of Mutual Interest (optional)
-          </label>
-          <input
-            type="text"
-            id="skills"
-            placeholder="e.g., JavaScript, UX Design"
-            value={skills}
-            onChange={(e) => setSkills(e.target.value)}
-            className="mt-1 block w-full border border-gray-300 rounded-lg p-2 bg-white focus:outline-none focus:ring-2 focus:ring-indigo-500"
-          />
-        </div> 
-       <div className="mt-4">
-          <label className="block text-sm font-medium text-gray-700">
-            Role Requirements
-          </label>
-          <input
-            type="text"
-            id="skills"
-            placeholder="e.g., Developer, Designer, Content Writer"
-            value={skills}
-            onChange={(e) => setSkills(e.target.value)}
-            className="mt-1 block w-full border border-gray-300 rounded-lg p-2 bg-white focus:outline-none focus:ring-2 focus:ring-indigo-500"
-          />
-        </div> 
-        <div className="mt-6 flex justify-end space-x-3">
-          <button
-            type="submit"
-            className="bg-indigo-600 text-white hover:bg-indigo-500 rounded-full px-5 py-2 shadow-md transition duration-300 ease-in-out transform hover:scale-105"
-          >
-            Send Request
-          </button>
-        </div>
-      </form>
-    </div>
-  </div>
-)}
-
-
+              {isDropdownOpen && (
+                <div
+                  id="dropdown"
+                  className="absolute right-0 z-10 mt-2 w-64 rounded-lg shadow-xl bg-gradient-to-br from-indigo-50 via-white to-indigo-100 ring-1 ring-indigo-200 ring-opacity-50 transition-transform duration-300 transform scale-95 origin-top-right"
+                >
+                  <div className="py-4 px-5 bg-white bg-opacity-90 rounded-lg">
+                    <form
+                      id="connectionForm"
+                      className="p-4 bg-white bg-opacity-90 rounded-lg shadow-md border border-gray-200"
+                      onSubmit={handleFormSubmit}
+                    >
+                      <textarea
+                        id="message"
+                        rows="4"
+                        maxLength="300"
+                        placeholder="Provide a brief description of the project..."
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                        className="w-full p-3 border border-gray-300 rounded-lg mt-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                      ></textarea>
+                      <div className="mt-4">
+                        <label className="block text-sm font-medium text-gray-700">
+                          Collaboration Purpose
+                        </label>
+                        <select
+                          id="purpose"
+                          value={purpose}
+                          onChange={(e) => setPurpose(e.target.value)}
+                          className="mt-1 block w-full border border-gray-300 rounded-lg p-2 bg-white focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                        >
+                          <option value="collaboration">Open-Source Project</option>
+                          <option value="skill-sharing">Hackathon Team</option>
+                          <option value="mentorship">Startup</option>
+                          <option value="networking">Research</option>
+                          <option value="discussion">Others</option>
+                        </select>
+                      </div>
+                      <div className="mt-4">
+                        <label className="block text-sm font-medium text-gray-700">
+                          Skills or Topics of Mutual Interest (optional)
+                        </label>
+                        <input
+                          type="text"
+                          id="skills"
+                          placeholder="e.g., JavaScript, UX Design"
+                          value={skills}
+                          onChange={(e) => setSkills(e.target.value)}
+                          className="mt-1 block w-full border border-gray-300 rounded-lg p-2 bg-white focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                        />
+                      </div>
+                      <div className="mt-6 flex justify-end space-x-3">
+                        <button
+                          type="submit"
+                          className="bg-indigo-600 text-white hover:bg-indigo-500 rounded-full px-5 py-2 shadow-md transition duration-300 ease-in-out transform hover:scale-105"
+                        >
+                          Send Request
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              )}
             </div>
           </div>
         </div>
@@ -209,86 +209,43 @@ function HeroSection() {
               <div className="h-3 w-3 rounded-full bg-green-200"></div>
             </div>
           </div>
-          <div className="overflow-hidden border-t-[2px] border-indigo-900 px-4 lg:px-8 py-4 lg:py-8">
-          <code className="font-mono text-xs md:text-sm lg:text-base">
-              <div className="blink">
-                <span className="mr-2 text-pink-500">const</span>
-                <span className="mr-2 text-white">coder</span>
-                <span className="mr-2 text-pink-500">=</span>
-                <span className="text-gray-400">{'{'}</span>
+          <div className="relative flex h-full w-full items-center justify-center pt-2 lg:pt-8">
+            <div className="absolute lg:-left-20 hidden lg:block">
+              <Image
+                src="/hero-profile.svg"
+                alt="Profile"
+                width={210}
+                height={695}
+                className="lg:h-96"
+              />
+            </div>
+            <div className="w-full px-6 lg:pr-20 flex flex-col items-center">
+              <Image
+                src={personalData.profilePic}
+                alt="Profile Picture"
+                width={280}
+                height={280}
+                className="relative z-10 mb-6 rounded-full object-cover shadow-lg"
+              />
+              <h2 className="text-2xl font-bold text-white sm:text-3xl lg:text-4xl">
+                {personalData.name}
+              </h2>
+              <p className="mt-2 text-sm text-gray-300 sm:text-base lg:text-lg">
+                {personalData.description}
+              </p>
+              <div className="mt-6 flex space-x-4">
+                <Link href="#resume">
+                  <button className="px-8 py-2 text-white bg-pink-500 rounded-full">
+                    Download Resume
+                  </button>
+                </Link>
+                <Link href="#projects">
+                  <button className="px-8 py-2 text-white bg-violet-600 rounded-full">
+                    View Projects
+                  </button>
+                </Link>
               </div>
-              <div>
-                <span className="ml-4 lg:ml-8 mr-2 text-white">name:</span>
-                <span className="text-gray-400">{''}</span>
-                <span className="text-amber-300">Abu Said</span>
-                <span className="text-gray-400">{','}</span>
-              </div>
-              <div className="ml-4 lg:ml-8 mr-2">
-                <span className=" text-white">skills:</span>
-                <span className="text-gray-400">{'['}</span>
-                <span className="text-amber-300">React</span>
-                <span className="text-gray-400">{"', '"}</span>
-                <span className="text-amber-300">NextJS</span>
-                <span className="text-gray-400">{"', '"}</span>
-                <span className="text-amber-300">Redux</span>
-                <span className="text-gray-400">{"', '"}</span>
-                <span className="text-amber-300">Express</span>
-                <span className="text-gray-400">{"', '"}</span>
-                <span className="text-amber-300">NestJS</span>
-                <span className="text-gray-400">{"', '"}</span>
-                <span className="text-amber-300">MySql</span>
-                <span className="text-gray-400">{"', '"}</span>
-                <span className="text-amber-300">MongoDB</span>
-                <span className="text-gray-400">{"', '"}</span>
-                <span className="text-amber-300">Docker</span>
-                <span className="text-gray-400">{"', '"}</span>
-                <span className="text-amber-300">AWS</span>
-                <span className="text-gray-400">{"'],"}</span>
-              </div>
-              <div>
-                <span className="ml-4 lg:ml-8 mr-2 text-white">hardWorker:</span>
-                <span className="text-orange-400">true</span>
-                <span className="text-gray-400">,</span>
-              </div>
-              <div>
-                <span className="ml-4 lg:ml-8 mr-2 text-white">quickLearner:</span>
-                <span className="text-orange-400">true</span>
-                <span className="text-gray-400">,</span>
-              </div>
-              <div>
-                <span className="ml-4 lg:ml-8 mr-2 text-white">problemSolver:</span>
-                <span className="text-orange-400">true</span>
-                <span className="text-gray-400">,</span>
-              </div>
-              <div>
-                <span className="ml-4 lg:ml-8 mr-2 text-green-400">hireable:</span>
-                <span className="text-orange-400">function</span>
-                <span className="text-gray-400">{'() {'}</span>
-              </div>
-              <div>
-                <span className="ml-8 lg:ml-16 mr-2 text-orange-400">return</span>
-                <span className="text-gray-400">{'('}</span>
-              </div>
-              <div>
-                <span className="ml-12 lg:ml-24 text-cyan-400">this.</span>
-                <span className="mr-2 text-white">hardWorker</span>
-                <span className="text-amber-300">&amp;&amp;</span>
-              </div>
-              <div>
-                <span className="ml-12 lg:ml-24 text-cyan-400">this.</span>
-                <span className="mr-2 text-white">problemSolver</span>
-                <span className="text-amber-300">&amp;&amp;</span>
-              </div>
-              <div>
-                <span className="ml-12 lg:ml-24 text-cyan-400">this.</span>
-                <span className="mr-2 text-white">skills.length</span>
-                <span className="mr-2 text-amber-300">&gt;=</span>
-                <span className="text-orange-400">5</span>
-              </div>
-              <div><span className="ml-8 lg:ml-16 mr-2 text-gray-400">{');'}</span></div>
-              <div><span className="ml-4 lg:ml-8 text-gray-400">{'};'}</span></div>
-              <div><span className="text-gray-400">{'};'}</span></div>
-            </code>
+            </div>
           </div>
         </div>
       </div>
---
+++ app/api/connect/route.ts
@@ -0,0 +1,50 @@
+// app/api/friend-request/send/route.ts
+import { NextRequest, NextResponse } from 'next/server';
+import { PrismaClient } from '@prisma/client';
+import { useSession }  from 'next-auth/react';
+import { useCurrentUser } from "../../../hooks/use-current-user"
+
+const prisma = new PrismaClient();
+
+export async function POST(req: NextRequest) {
+  try {
+  
+   
+   
+    const { receiverId,senderId } = await req.json();
+   console.log(receiverId,senderId)
+     if (!senderId) {
+      return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+    }
+    if (senderId === receiverId) {
+      return NextResponse.json({ message: 'Cannot send friend request to yourself' }, { status: 400 });
+    }
+
+    const existingRequest = await prisma.friendRequest.findUnique({
+      where: {
+        senderId_receiverId: {
+          senderId: senderId,
+          receiverId: receiverId,
+        },
+      },
+    });
+
+    if (existingRequest) {
+      return NextResponse.json({ message: 'Friend request already exists' }, { status: 400 });
+    }
+
+    const friendRequest = await prisma.friendRequest.create({
+      data: {
+        senderId: senderId,
+        receiverId: receiverId,
+      },
+    });
+
+    return NextResponse.json(friendRequest, { status: 201 });
+  } catch (error) {
+    console.error('Error sending friend request:', error);
+    return NextResponse.json({ message: 'Error sending friend request' }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+}
