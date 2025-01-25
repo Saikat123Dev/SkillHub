d74bf409ed1da524fa856200e3343d601edb3516
Saikat123Dev <berasaikat729@gmail.com>
2025-01-25 21:52:50 +0530

    next conf correct

--- APP/next.config.js
+++ APP/next.config.js
@@ -1,50 +1,50 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: 'standalone',
-//   eslint: {
-//     // Warning: This allows production builds to successfully complete even if
-//     // your project has ESLint errors.
-//     ignoreDuringBuilds: true,
-//   },
-//   typescript: {
-//     // !! WARN !!
-//     // Dangerously allow production builds to successfully complete even if
-//     // your project has type errors.
-//     // !! WARN !!
-//     ignoreBuildErrors: true,
-//   },
-// }
-
-// module.exports = nextConfig
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = {
-  ...nextConfig,
+const nextConfig = {
+  output: 'standalone',
   eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-  reactStrictMode: true,
   typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
     ignoreBuildErrors: true,
   },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'media.dev.to',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'utfs.io',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'assets.aceternity.com', // Added this entry
-        pathname: '/**',
-      },
-    ],
-  },
-};
+}
+
+// module.exports = nextConfig
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// module.exports = {
+//   ...nextConfig,
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   reactStrictMode: true,
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'http',
+//         hostname: 'media.dev.to',
+//         pathname: '/**',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'utfs.io',
+//         pathname: '/**',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'assets.aceternity.com', // Added this entry
+//         pathname: '/**',
+//       },
+//     ],
+//   },
+// };
